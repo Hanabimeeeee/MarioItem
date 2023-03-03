30cc008910b0755a234626877fac9c552440283a
Hanabimeeeee <Phichitphong757@gmail.com>
2023-03-03 14:54:43 +0700

    first init

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        Mushroom m1 = new Mushroom();
+        m1.show();
+
+        int[] distance = new int[]{10,3};
+        m1.run(new int[]{7, 3});
+    
+    }
+}
---
+++ src/Mushroom.java
@@ -0,0 +1,25 @@
+public class Mushroom extends Item{
+    
+    @Override
+    public void show(){
+        setAppear(true);
+        System.out.println("Block is cracked! Mushroom is appearing!");
+    }
+
+      /**
+     * 
+     * @param distance between current location to new location
+     */
+    public void run(int[] distance){
+        if(getAppear()){
+            int[] currentLocation = getLocation();
+            System.out.println("Mushroom is at "+ currentLocation[0]+ "," + currentLocation[1]);
+            System.out.println("Mushroom is runing!");
+            currentLocation[0] = currentLocation[0]+ distance[0];
+            currentLocation[1] = currentLocation[1]- distance[1];
+            setLocation(currentLocation);
+            System.out.println("Mushroom is at "+currentLocation[0]+","+currentLocation[1]);
+        }else {System.out.println("Mushroom is disappear!");}
+    }
+
+}
